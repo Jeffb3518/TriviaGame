a336545d516882c633a69737f83b37cd073ff379
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2016-10-21 11:53:51 -0400

    HTML & JS update

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var count = 60;
+var counter=setInterval(timer, 1000);
+
+function timer() {
+count=count-1;
+	if (count <=0) {
+		clearInterval(counter);
+		return;
+	}
+document.getElementById("timer").innerHTML=count + " secs";
+}
+
+
