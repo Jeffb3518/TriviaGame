60fc6acd0f2ea75cfd8839574fce1e28ae84abb7
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2016-10-21 15:46:04 -0400

    HTML & JS Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,4 +10,22 @@ count=count-1;
 document.getElementById("timer").innerHTML=count + " secs";
 }
 
+var answers = ["C", "A", "A", "C", "B"],
+	tot = answers.length
 
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName( radioName ); 
+    for(var y=0; y<radios.length; y++)
+      if(radios[y].checked) return radios[y].value; 
+}
+
+function getScore(){
+  var score = 0;
+  for (var i=0; i<tot; i++)
+    if(getCheckedValue("question"+i)===answers[i]) score += 1; 
+  return score;
+}
+
+function returnScore(){
+  alert("Correct Answers: "+ getScore());
+}
